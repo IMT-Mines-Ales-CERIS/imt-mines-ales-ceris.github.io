7f6189000764735ebf8fddb1a8d87526e2c3b416
pajean <pierre-antoine.jean@mines-ales.fr>
2025-05-27 09:29:22 +0200

    Add logo sycoia.

--- js/header.js
+++ js/header.js
@@ -4,6 +4,7 @@
     let nbFolders = currentPath.split('/').length - 1;
     let imagePath = `${"../".repeat(nbFolders)}images/logo-hceres.png`;
     let faviconPath = `${"../".repeat(nbFolders)}images/favicon.png`;
+    let imageSycoiaPath = `${"../".repeat(nbFolders)}images/logo_unite.png`;
     let tagContent = `
         <div class="bg-image-index padding-image-index">
             <div class="container d-flex flex-column">
@@ -11,8 +12,9 @@
                     <h1 class="fw-light">Portfolio SyCoIA</h1>
                     <p class="lead text-body-light">Systèmes Complexes et Intelligence Artificielle</p>
                 </div>
-                <div id="logo">
-                    <img src="${imagePath}" class="img-fluid w-25" alt="...">
+                <div id="logo" class="mt-4">
+                    <img src="${imagePath}" class="img-fluid me-4" style="width: 20%" alt="...">
+                    <img src="${imageSycoiaPath}" class="img-fluid" style="border-radius:50%; width: 20%" alt="...">
                 </div>
             </div>
         </div>`;
